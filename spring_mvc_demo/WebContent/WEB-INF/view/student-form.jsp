e53ee0a88b7e2e6874b08e5da6465099c56d88d3
davidkeresztesi <keresztesidavid47@gmail.com>
2022-12-12 14:06:53 +0100

    first bulk commit

---
+++ spring_mvc_demo/WebContent/WEB-INF/view/student-form.jsp
@@ -0,0 +1,58 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Student Registration Form</title>
+</head>
+
+<body>
+
+
+	<form:form action="processForm" modelAttribute="student">
+
+		First name: <form:input path="firstName" />
+		
+		<br><br>
+	
+		Last name: <form:input path="lastName" />
+		
+		<br><br>		
+		
+		<form:select path="country">
+		
+		Nationality:
+		
+		<form:options items="${student.countryOptions}" />
+				
+		</form:select>
+		
+		<br><br>
+		
+		Favorite Language:
+		
+		Java <form:radiobutton path="favoriteLanguage" value="Java" />
+		C# <form:radiobutton path="favoriteLanguage" value="C#" />
+		PHP <form:radiobutton path="favoriteLanguage" value="PHP" />
+		Ruby <form:radiobutton path="favoriteLanguage" value="Ruby" />
+					
+		<br><br>
+		
+		Operating Systems:
+		
+		Windows <form:checkbox path="operatingSystem" value="Windows"/>
+		MacOS <form:checkbox path="operatingSystem" value="MacOS"/>
+		Linux <form:checkbox path="operatingSystem" value="Linux"/>
+		
+		<br><br>
+	
+		<input type="submit" value="Submit" />
+
+	</form:form>
+
+
+</body>
+
+</html>
---
+++ spring_mvc_demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -0,0 +1,80 @@
+package com.luv2code.springdemo.mvc;
+
+import java.util.LinkedHashMap;
+
+public class Student {
+	
+	private String firstName;
+	private String lastName;
+	
+	private String country;
+	
+	private LinkedHashMap<String, String> countryOptions;
+	
+	private String favoriteLanguage;
+	
+	private String[] operatingSystem;
+	
+	public Student() {
+		
+		//populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<String, String>();
+		
+		countryOptions.put("HUN", "Hungary");
+		countryOptions.put("AUT", "Austria");
+		countryOptions.put("SVK", "Slovakia");
+		countryOptions.put("UKR", "Ukraine");
+		countryOptions.put("ROU", "Romania");
+		countryOptions.put("SRB", "Serbia");
+		countryOptions.put("HRV", "Croatia");
+		countryOptions.put("SVN", "Slovenia");	
+		
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystem() {
+		return operatingSystem;
+	}
+
+	public void setOperatingSystem(String[] operatingSystem) {
+		this.operatingSystem = operatingSystem;
+	}
+
+	
+}
